612a9de24bce6973c31fb2efe5c5b806e12b7618
Gordon <gordonspencer.c@gmail.com>
2019-10-12 15:18:16 -0400

    added global error handler
    Co-authored-by: MichaelR liikemike@aim.com

--- server/server.js
+++ server/server.js
@@ -20,4 +20,18 @@ app.use('/build', express.static(path.resolve(__dirname, '../build')));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../client/index.html')));
 
+app.all('*', (req, res) => {
+  res.status(404).send('Page not found assholes!');
+});
+
+app.use('/', (err, req, res, next) => {
+  const defaultError = {
+    status: 500,
+    message: 'express error caught unknown middleware error'
+  };
+  const newError = { ...defaultError, ...err };
+  console.log('This is global error handler: ', newError.message);
+  res.status(newError.status).send(newError.message);
+});
+
 app.listen(PORT, () => console.log(`you are being heard on port ${PORT}`));
