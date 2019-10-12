3a2b3201eb8ee92c4f86d461f91f1ddb1f9a3fe2
MichaelRomero <liikemike@aim.com>
2019-10-12 18:13:58 -0400

    added #getPets inside petController: successfully queries pets from database added #getPets to petsRouter under the GET / endpoint

--- server/controllers/petsController.js
+++ server/controllers/petsController.js
@@ -1,3 +1,18 @@
 const petsController = {};
+const db = require('../../database/database');
+
+/**
+ * @description gets all Pets from a single user(owner)
+ * @requirements : a owner_id stored inside res.locals
+ */
+petsController.getPets = (req, res, next) => {
+  console.log('\n*********** petsController.getPets ****************', `\nMETHOD: ${req.method} \nENDPOINT: '${req.url}' \nBODY: ${JSON.stringify(req.body)} \nLOCALS: ${JSON.stringify(res.locals)} `);
+
+  db.query('SELECT * FROM pets WHERE owner_id = $1', [1], (err, response) => {
+    if (err) return next(err);
+    res.locals.pets = response.rows;
+    return next();
+  });
+};
 
 module.exports = petsController;
--- server/routes/petsRouter.js
+++ server/routes/petsRouter.js
@@ -2,5 +2,14 @@ const router = require('express').Router();
 
 const petsController = require('../controllers/petsController');
 
+/**
+ * @endpoint : '/pets/'
+ * @method : GET
+ * @returns -> an array of all pets via JSON
+ */
+router.get('/', petsController.getPets, (req, res) => {
+  const { pets } = res.locals;
+  res.json(pets);
+});
 
 module.exports = router;
