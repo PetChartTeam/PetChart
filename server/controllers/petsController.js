bd5dfbaae7a4ec6c1c94c41438dc5e365ba93cb4
Gordon <gordonspencer.c@gmail.com>
2019-10-12 14:59:59 -0400

    connect to db, setup routers, setup controllers.
    Co-authored-by: Michael R liikemike@aim.com

---
+++ server/controllers/petsController.js
@@ -0,0 +1,3 @@
+const petsController = {};
+
+module.exports = petsController;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const path = require('path');
+// const db = require('../database/database.js');
+const accountsRouter = require('./routes/accountsRouter');
+const petsRouter = require('./routes/petsRouter');
 
 const app = express();
 
 const PORT = 3000;
+
+// db.query('SELECT * FROM pets', (err, res) => {
+//   console.log('this is the db response: ', res.rows);
+// });
+
+app.use('/accounts', accountsRouter);
+
+app.use('/pets', petsRouter);
 
 app.use('/build', express.static(path.resolve(__dirname, '../build')));
 
