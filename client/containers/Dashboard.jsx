3c221a820d8a76e896daf90b3a8812d8a45d08f8
Tom Herrmann <tomherrmanndev@gmail.com>
2019-10-15 14:54:59 -0400

    note about refactoring to-do

--- client/containers/Dashboard.jsx
+++ client/containers/Dashboard.jsx
@@ -19,17 +19,17 @@ import Home from '../components/Home.jsx';
 import Nav from '../components/Nav.jsx';
 import Profile from '../components/Profile.jsx';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   userProfile: state.app.userProfile,
   dashboardPage: state.app.dashboardPage,
   activePet: state.app.activePet,
-  appPage: state.app.appPage,
+  appPage: state.app.appPage
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   changeDBPage: (pageName, activePet) => dispatch(actions.changeDBPage(pageName, activePet)),
-  savePet: (petProfile) => dispatch(actions.savePet(petProfile)),
-  logout: (newPage) => dispatch(actions.logout(newPage)),
+  savePet: petProfile => dispatch(actions.savePet(petProfile)),
+  logout: newPage => dispatch(actions.logout(newPage))
 });
 
 class Dashboard extends Component {
@@ -75,15 +75,29 @@ class Dashboard extends Component {
   render() {
     // console.log('Dashboard petNavDetails: ', this.petNavDetails);
     // set default childPage to Home component
-    const homeComponent = <Home changeDBPage={this.props.changeDBPage} pets={this.props.userProfile.pets} activatePet={this.activatePet} />;
+    const homeComponent = (
+      <Home
+        changeDBPage={this.props.changeDBPage}
+        pets={this.props.userProfile.pets}
+        activatePet={this.activatePet}
+      />
+    );
     let childPage = homeComponent;
     // check if state dashboardPage is home or profile and render corresponding component
     switch (this.props.dashboardPage) {
       case 'home':
+        // MOVE AND COMBINE LINES 78 & 79 TO REPLACE THE LINE BELOW
         childPage = homeComponent;
         break;
       case 'profile':
-        childPage = <Profile changeDBPage={this.props.changeDBPage} activePet={this.props.activePet} savePet={this.props.savePet} ownerID={this.props.userProfile.owner.id} />;
+        childPage = (
+          <Profile
+            changeDBPage={this.props.changeDBPage}
+            activePet={this.props.activePet}
+            savePet={this.props.savePet}
+            ownerID={this.props.userProfile.owner.id}
+          />
+        );
         break;
       default:
         break;
@@ -91,11 +105,20 @@ class Dashboard extends Component {
     return (
       // always render Nav component and whatever childPage is set to
       <div>
-        <Nav logout={this.props.logout} changeDBPage={this.props.changeDBPage} activatePet={this.activatePet} owner={this.props.userProfile.owner.firstName} pets={this.props.userProfile.pets} />
+        <Nav
+          logout={this.props.logout}
+          changeDBPage={this.props.changeDBPage}
+          activatePet={this.activatePet}
+          owner={this.props.userProfile.owner.firstName}
+          pets={this.props.userProfile.pets}
+        />
         {childPage}
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
